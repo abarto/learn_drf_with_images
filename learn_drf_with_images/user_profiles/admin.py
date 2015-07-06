431ef8fb646414845e49d6bd227d9f04f60bfe08
Agustin Barto <abarto@gmail.com>
2015-07-06 10:41:54 -0300

    Add user profile admin inline

--- learn_drf_with_images/user_profiles/admin.py
+++ learn_drf_with_images/user_profiles/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
 
-# Register your models here.
+from .models import UserProfile
+
+
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    max_num = 1
+    can_delete = False
+
+
+class UserAdmin(DefaultUserAdmin):
+    def add_view(self, *args, **kwargs):
+        self.inlines = ()
+
+        return super(UserAdmin, self).add_view(*args, **kwargs)
+
+    def change_view(self, *args, **kwargs):
+        self.inlines = (UserProfileInline,)
+
+        return super(UserAdmin, self).change_view(*args, **kwargs)
+
+
+admin.site.unregister(get_user_model())
+admin.site.register(get_user_model(), UserAdmin)
