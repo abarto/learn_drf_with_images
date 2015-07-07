b78f575e6ab0f111311a55b09efd22e937a133c2
Agustin Barto <abarto@gmail.com>
2015-07-06 22:36:39 -0300

    Fix serializer

--- learn_drf_with_images/user_profiles/serializers.py
+++ learn_drf_with_images/user_profiles/serializers.py
@@ -5,5 +5,5 @@ from .models import UserProfile
 class UserProfileSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = UserProfile
-        fields = ('url', 'date_of_birth', 'phone_number', 'gender')
+        fields = ('url', 'date_of_birth', 'phone_number', 'gender', 'image')
         readonly_fields = ('url', 'image')
